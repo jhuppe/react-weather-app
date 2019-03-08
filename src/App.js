5f88016f09751f8f9ad1b87ccfa4a1e492ae6b05
Jeremy Huppe <jeremyhuppe@Jeremys-MacBook-Air.local>
2019-03-07 19:50:46 -0700

    Update api call to serve over https

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const units = 'imperial';
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=${units}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=${units}`);
     const response = await api_call.json();
 
     if (city && country) {
