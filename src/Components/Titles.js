e625fd613d7a3b15312a96265d4941d572243d7d
Jeremy Huppe <jeremyhuppe@Jeremys-MacBook-Air.local>
2019-03-11 09:18:29 -0600

    Update app name

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = (props) => {
     return (
         <div>
-            <h1 className="title-container__title">Weather App</h1>
+            <h1 className="title-container__title">Instant Weather</h1>
             <p className="title-container__subtitle">Find weather conditions in cities across the globe.</p>
         </div>
     )
